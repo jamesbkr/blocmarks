6e53f4f93184eb75f97f1cdf84ce7ec6b6511c74
James Becker <becker.james90@gmail.com>
2015-10-06 19:21:17 +0000

    basic devise and email confirmation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,17 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.action_mailer.default_url_options = {host: 'blocmarks-jb.herokuapp.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+     :address        => "smtp.sendgrid.net",
+     :port           => "25",
+     :authentication => :plain,
+     :user_name      => ENV['SENDGRID_USERNAME'],
+     :password       => ENV['SENDGRID_PASSWORD'],
+     :domain         => ENV['SENDGRID_DOMAIN']
+    }
+  
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
